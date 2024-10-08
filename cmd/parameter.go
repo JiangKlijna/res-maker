37a822e7ba2c58bcfdb82229556a8d13619cdcbf
jiangKlijna <leil729564090@outlook.com>
2024-10-08 20:24:40 +0800

    0.2 refine parameter

--- cmd/parameter.go
+++ cmd/parameter.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/jiangklijna/res-maker/def"
@@ -26,8 +27,10 @@ type parameter struct {
 	duration   string
 	configFile string
 
-	cpuNs [24]uint
-	memNs [24]uint
+	timeDuration time.Duration
+
+	cpuNs *[24]uint
+	memNs *[24]uint
 }
 
 // GetParameter get and check parameter
@@ -35,7 +38,7 @@ func GetParameter() Parameter {
 	p := new(parameter)
 	flag.IntVar(&p.nCores, "c", 0, "Number of cores to use")
 	flag.IntVar(&p.nMemGs, "m", 0, "Size of memory to allocate in GB")
-	flag.StringVar(&p.duration, "d", "", "Specify the duration (e.g., 1s, 1d, 3m) for the program to run")
+	flag.StringVar(&p.duration, "d", "", "Specify the duration (e.g., 1s, 2m, 3h, 4d) for the program to run")
 	flag.StringVar(&p.configFile, "f", "", "Specify the configuration file for the program.")
 
 	help := flag.Bool("h", false, "this help")
@@ -60,22 +63,42 @@ func (p *parameter) init() {
 		if err != nil {
 			panic(err)
 		}
-		c := new(configFile)
-		err = json.Unmarshal(content, c)
+		config := make(map[string]map[string]*uint)
+		err = json.Unmarshal(content, &config)
 		if err != nil {
 			panic(err)
 		}
-		p.cpuNs = convert(c.Cpu)
-		p.memNs = convert(c.Mem)
+		if m, isOk := config[def.Cpu.Name()]; isOk {
+			p.cpuNs = convert(m)
+		}
+		if m, isOk := config[def.Mem.Name()]; isOk {
+			p.memNs = convert(m)
+		}
+		if p.cpuNs == nil && p.memNs == nil {
+			fmt.Println(time.Now().Format(time.DateTime), "Error: config[", p.configFile, "], cpu or mem is not null")
+			os.Exit(1)
+		}
 	} else {
 		if p.nCores < 0 {
-			fmt.Println("Warning: cpu should be >= 0")
+			fmt.Println(time.Now().Format(time.DateTime), "Warning: cpu should be >= 0")
 			p.nCores = 0
 		}
 		if p.nMemGs < 0 {
-			fmt.Println("Warning: mem should be >= 0")
+			fmt.Println(time.Now().Format(time.DateTime), "Warning: mem should be >= 0")
 			p.nMemGs = 0
 		}
+		if p.nCores == 0 && p.nMemGs == 0 {
+			fmt.Println(time.Now().Format(time.DateTime), "Error: cpu or mem must be >= 0")
+			os.Exit(1)
+		}
+	}
+	if len(p.duration) > 0 {
+		d, err := parseDuration(p.duration)
+		if err != nil {
+			fmt.Println(time.Now().Format(time.DateTime), "Error:")
+			panic(err)
+		}
+		p.timeDuration = d
 	}
 }
 
@@ -85,31 +108,40 @@ func (p *parameter) IsConfigFile() bool {
 
 func (p *parameter) GetConfigFile() map[def.ResType][24]uint {
 	m := make(map[def.ResType][24]uint)
-	m[def.Cpu] = p.cpuNs
-	m[def.Mem] = p.memNs
+	if p.cpuNs != nil {
+		m[def.Cpu] = *p.cpuNs
+	}
+	if p.memNs != nil {
+		m[def.Mem] = *p.memNs
+	}
+	if len(m) == 0 {
+		panic("config file error")
+	}
 	return m
 }
 
 func (p *parameter) GetParameter() map[def.ResType]uint {
 	m := make(map[def.ResType]uint)
-	m[def.Cpu] = uint(p.nCores)
-	m[def.Mem] = uint(p.nMemGs)
+	if p.nCores > 0 {
+		m[def.Cpu] = uint(p.nCores)
+	}
+	if p.nMemGs > 0 {
+		m[def.Mem] = uint(p.nMemGs)
+	}
+	if len(m) == 0 {
+		panic("parameter error")
+	}
 	return m
 }
 
 func (p *parameter) GetDuration() time.Duration {
-	return 0
+	return p.timeDuration
 }
 
-type configFile struct {
-	Cpu map[string]*uint `json:"cpu"`
-	Mem map[string]*uint `json:"mem"`
-}
-
-func convert(m map[string]*uint) [24]uint {
+func convert(m map[string]*uint) *[24]uint {
 	var arr [24]uint
 	if len(m) == 0 {
-		return arr
+		return &arr
 	}
 	var pre uint
 	for i := 23; i >= 0; i-- {
@@ -126,7 +158,7 @@ func convert(m map[string]*uint) [24]uint {
 		}
 		arr[i] = pre
 	}
-	return arr
+	return &arr
 }
 
 func formatIndex(i int) string {
@@ -135,4 +167,31 @@ func formatIndex(i int) string {
 		return "0" + s
 	}
 	return s
+}
+
+// parseDuration parses strings like "1s", "2m", "3h", "4d" and returns the corresponding time.Duration.
+func parseDuration(input string) (time.Duration, error) {
+	// Remove the last character which represents the unit
+	valueStr := strings.TrimRight(input, "dsmh")
+	unit := strings.ToLower(input[len(valueStr):])
+
+	// Convert the string value to an integer
+	value, err := strconv.Atoi(valueStr)
+	if err != nil {
+		return 0, fmt.Errorf("-d invalid input: %s", input)
+	}
+
+	// Calculate the duration based on the unit
+	switch unit {
+	case "", "s":
+		return time.Duration(value) * time.Second, nil
+	case "m":
+		return time.Duration(value) * time.Minute, nil
+	case "h":
+		return time.Duration(value) * time.Hour, nil
+	case "d":
+		return time.Duration(value) * 24 * time.Hour, nil
+	default:
+		return 0, fmt.Errorf("-d unsupported unit: %s", unit)
+	}
 }
